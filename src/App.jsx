34094183e8b0b9fd4f6d456bfb4edb8056139e86
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-05-16 12:05:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            triggered: false // when true, the modal appears
+        }
+    }
+    render = () => {
+        let modal = undefined
+        if (this.state.triggered) {
+            // modal is reassigned if triggered is true
+            modal = (<div className='modal-body'>
+                <div className="modal-container">
+                    modal text
+                    <button onClick={this.toggleModal}>close modal</button>
+                </div>
+            </div>)
+        }
+        // If modal is undefined, no modal appears
+        // Otherwise show the modal
+        return (<div>
+            {modal}
+            initial text
+            <button onClick={this.toggleModal}>open modal</button>
+        </div>)
+    }
+    toggleModal = () => {
+        this.setState({ triggered: !this.state.triggered })
+    }
+}
+export default App
